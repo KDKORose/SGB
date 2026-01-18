0ddc301c64320be0cb8612a6e052aa88b706208e
KDKO <kdko.business@gmail.com>
2026-01-18 00:18:54 -0500

    Testing cog

--- cogs/dev.py
+++ cogs/dev.py
@@ -5,11 +5,9 @@ class Dev(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @commands.hybrid_command(name="ping")
+    @commands.hybrid_command(name="ping",  with_app_command=True, description="Ping the bot!")
     async def ping(self, ctx: commands.Context):
-        if isinstance(ctx, discord.Interaction):
-            await ctx.send(f"Pong! Latency of {self.bot.latency()}.\nNote: Slash Command!")
-        await ctx.send(f"Pong! Latency of {self.bot.latency()}.\n Note: Prefix Command!")
+        await ctx.reply(f"Pong! Latency of {self.bot.latency()}.\n Note: Prefix Command!")
     
 async def setup(bot: commands.Bot):
     await bot.add_cog(Dev(bot))
