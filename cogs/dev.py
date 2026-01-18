4501dd7c17546fab1dd777dae6febb5fedf26ffe
KDKO <kdko.business@gmail.com>
2026-01-18 00:33:55 -0500

    e

--- cogs/dev.py
+++ cogs/dev.py
@@ -4,9 +4,10 @@ class Dev(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @commands.hybrid_command(name="ping",  with_app_command=True, description="Ping the bot!")
-    async def ping(self, ctx: commands.Context):
-        await ctx.reply(f"Pong! Latency of {self.bot.latency()}.\n Note: Prefix Command!")
+    @commands.hybrid_command(name="ping",  description="Ping the bot!")
+    async def ping(self, ctx):
+        print("Ping command called!")
+        await ctx.send(f"Pong! Latency of {self.bot.latency()}.\n Note: Prefix Command!")
     
 async def setup(bot: commands.Bot):
     await bot.add_cog(Dev(bot))
