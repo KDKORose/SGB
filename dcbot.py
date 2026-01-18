93733debaf71204e8af3f00247d0bddbc7f4f71f
KDKO <kdko.business@gmail.com>
2026-01-17 21:03:25 -0500

    Super not defined

--- dcbot.py
+++ dcbot.py
@@ -12,7 +12,7 @@ class DCBot(commands.Bot):
         self.DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
         self.BOT_PREFIX = os.getenv("BOT_PREFIX")
 
-        super.__init__(command_prefix=self.BOT_PREFIX, intents=discord.Intents.all())
+        super().__init__(command_prefix=self.BOT_PREFIX, intents=discord.Intents.all())
         self.app = app # Access other services
     
     async def on_ready(self):
